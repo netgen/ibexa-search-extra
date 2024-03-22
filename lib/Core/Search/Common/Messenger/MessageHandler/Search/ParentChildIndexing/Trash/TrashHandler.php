047fd324b1b1f1f63b88ee79664c3262e561f6d9
Katarina Miočić <katarina.m@netgen.io>
2024-03-22 15:52:49 +0100

    NGSTACK-836 parent child indexer

---
+++ lib/Core/Search/Common/Messenger/MessageHandler/Search/ParentChildIndexing/Trash/TrashHandler.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\IbexaSearchExtra\Core\Search\Common\Messenger\MessageHandler\Search\ParentChildIndexing\Trash;
+
+use Netgen\IbexaSearchExtra\Core\Search\Common\Messenger\MessageHandler\Search\ParentChildIndexing\AncestorIndexer;
+use Ibexa\Contracts\Core\Persistence\Content\Location\Handler as LocationHandler;
+use Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException;
+use Netgen\IbexaSearchExtra\Core\Search\Common\Messenger\Message\Search\Trash\Trash;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+
+use function sprintf;
+
+final class TrashHandler
+{
+    public function __construct(
+        private readonly LocationHandler $locationHandler,
+        private readonly AncestorIndexer $ancestorIndexer,
+        private readonly LoggerInterface $logger = new NullLogger(),
+    ) {}
+
+    public function __invoke(Trash $message): void
+    {
+        try {
+            $location = $this->locationHandler->load(
+                $message->parentLocationId,
+            );
+        } catch (NotFoundException) {
+            $this->logger->info(
+                sprintf(
+                    '%s: Location #%d is gone, aborting',
+                    $this::class,
+                    $message->parentLocationId,
+                ),
+            );
+
+            return;
+        }
+
+        $this->ancestorIndexer->indexSingle($location);
+    }
+}
