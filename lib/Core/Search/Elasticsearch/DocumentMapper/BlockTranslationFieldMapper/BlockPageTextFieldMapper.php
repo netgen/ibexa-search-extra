bca6ae440483a30b4cd4e2613fab345395457600
Katarina Miočić <katarina.m@netgen.io>
2024-05-09 10:50:24 +0200

    NGSTACK-843 configuration modifications

--- lib/Core/Search/Elasticsearch/DocumentMapper/BlockTranslationFieldMapper/BlockPageTextFieldMapper.php
+++ lib/Core/Search/Elasticsearch/DocumentMapper/BlockTranslationFieldMapper/BlockPageTextFieldMapper.php
@@ -4,6 +4,7 @@ namespace Netgen\IbexaSearchExtra\Core\Search\Elasticsearch\DocumentMapper\Block
 
 use Ibexa\Contracts\Core\Persistence\Content as SPIContent;
 use Ibexa\Contracts\Core\Persistence\Content\Type\Handler as ContentTypeHandler;
+use Ibexa\Contracts\Core\Persistence\Content\Location\Handler as LocationHandler;
 use Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException;
 use Ibexa\Contracts\Core\Search\Field;
 use Ibexa\Contracts\Core\Search\FieldType\FullTextField;
@@ -12,13 +13,9 @@ use Netgen\IbexaSearchExtra\Core\Search\Elasticsearch\DocumentMapper\BlockTransl
 
 class BlockPageTextFieldMapper extends BlockTranslationFieldMapper
 {
-    /**
-     * @param array<string> $allowedContentTypes
-     */
     public function __construct(
         private readonly PageTextExtractor $pageTextExtractor,
         private readonly ContentTypeHandler $contentTypeHandler,
-        private readonly array $allowedContentTypes,
     ) {}
 
     public function accept(SPIContent $content, string $languageCode): bool
@@ -31,10 +28,11 @@ class BlockPageTextFieldMapper extends BlockTranslationFieldMapper
      */
     public function mapFields(SPIContent $content, string $languageCode): array
     {
+        $siteConfig = $this->pageTextExtractor->getSiteConfigForContent($content->versionInfo->contentInfo->id);
         $fields = [];
         $contentType = $this->contentTypeHandler->load($content->versionInfo->contentInfo->contentTypeId);
 
-        if (in_array($contentType->identifier, $this->allowedContentTypes, true)) {
+        if (in_array($contentType->identifier, $siteConfig['allowed_content_types'], true)) {
             $text = $this->pageTextExtractor->extractPageText($content->versionInfo->contentInfo->id, $languageCode);
             foreach ($text as $level => $value) {
                 $fields[] = new Field('page_text_' . $level, $value, new FullTextField());
