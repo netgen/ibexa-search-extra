e22ccd6b870c88be3cccb8e4bbd40b0d8f30e71c
Petar Španja <petar@spanja.info>
2024-05-31 11:26:58 +0200

    NGSTACK-836: fix CS

--- lib/Container/Compiler/ParentChildIndexingPass.php
+++ lib/Container/Compiler/ParentChildIndexingPass.php
@@ -12,7 +12,7 @@ class ParentChildIndexingPass implements CompilerPassInterface
     private const ParentChildIndexerTag = 'netgen.parent_child_indexer.message_handler';
     private const MessageHandlerTag = 'messenger.message_handler';
 
-    public function process(ContainerBuilder $container) :void
+    public function process(ContainerBuilder $container): void
     {
         $useParentChildIndexing = $container->getParameter(
             'netgen_ibexa_search_extra.use_parent_child_indexing',
