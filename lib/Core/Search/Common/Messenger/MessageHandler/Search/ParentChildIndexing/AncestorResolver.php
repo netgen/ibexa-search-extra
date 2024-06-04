eac31db978b1376890a33c120d562684711dc0b7
Petar Španja <petar@spanja.info>
2024-06-04 09:56:14 +0200

    NGSTACK-836: extract AncestorPathGenerator with test

--- lib/Core/Search/Common/Messenger/MessageHandler/Search/ParentChildIndexing/AncestorResolver.php
+++ lib/Core/Search/Common/Messenger/MessageHandler/Search/ParentChildIndexing/AncestorResolver.php
@@ -9,13 +9,9 @@ use Ibexa\Contracts\Core\Persistence\Content\Location;
 use Ibexa\Contracts\Core\Persistence\Content\Location\Handler as LocationHandler;
 use Ibexa\Contracts\Core\Persistence\Content\Type\Handler as ContentTypeHandler;
 use Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException;
-use function array_merge;
-use function array_shift;
-use function count;
+
 use function end;
-use function explode;
 use function implode;
-use function is_array;
 use function is_int;
 use function mb_strlen;
 use function str_starts_with;
@@ -23,23 +19,15 @@ use function str_starts_with;
 final class AncestorResolver
 {
     /**
-     * @var string[]|null
-     */
-    private ?array $paths = null;
-
-    /**
      * @var array<int, string>
      */
     private array $contentIdContentTypeIdentifierCache = [];
 
-    /**
-     * @param array<string, mixed> $configuration
-     */
     public function __construct(
         private readonly ContentHandler $contentHandler,
         private readonly ContentTypeHandler $contentTypeHandler,
         private readonly LocationHandler $locationHandler,
-        private readonly array $configuration,
+        private readonly AncestorPathGenerator $ancestorPathGenerator,
     ) {}
 
     public function resolveAncestor(Location $location): ?Location
@@ -68,7 +56,7 @@ final class AncestorResolver
             return null;
         }
 
-        foreach ($this->getPaths() as $path) {
+        foreach ($this->ancestorPathGenerator->getPaths() as $path) {
             if (str_ends_with($path, $contentTypeIdentifier)) {
                 return $location;
             }
@@ -85,10 +73,12 @@ final class AncestorResolver
     private function matchPath(array $ancestry): false|int
     {
         $ancestryPath = $this->getAncestryPath($ancestry);
+
         if ($ancestryPath === null) {
             return false;
         }
-        foreach ($this->getPaths() as $path) {
+
+        foreach ($this->ancestorPathGenerator->getPaths() as $path) {
             if (str_starts_with($path, $ancestryPath)) {
                 return mb_strlen($path) - mb_strlen($ancestryPath);
             }
@@ -161,123 +151,5 @@ final class AncestorResolver
         }
 
         return $this->contentIdContentTypeIdentifierCache[$contentId];
-    }
-
-    /**
-     * @return string[]
-     */
-    private function getPaths(): array
-    {
-        if ($this->paths === null) {
-            $normalizedConfiguration = $this->normalizeConfiguration($this->configuration);
-            $paths = $this->recursiveFlattenPaths($normalizedConfiguration);
-            $this->paths = $this->expandPaths($paths);
-        }
-
-        return $this->paths;
-    }
-
-    /**
-     * @param string[] $paths
-     *
-     * @return string[]
-     */
-    private function expandPaths(array $paths): array
-    {
-        $expandedPathsGrouped = [[]];
-
-        foreach ($paths as $path) {
-            $expandedPathsGrouped[] = $this->recursiveExpandPath(explode('/', $path));
-        }
-
-        return array_merge(...$expandedPathsGrouped);
-    }
-
-    /**
-     * @param string[] $pathElements
-     *
-     * @return string[]
-     */
-    private function recursiveExpandPath(array $pathElements): array
-    {
-        $expandedPaths = [];
-
-        if (count($pathElements) > 1) {
-            $path = implode('/', $pathElements);
-            array_shift($pathElements);
-
-            $expandedPaths = [
-                $path,
-                ...$expandedPaths,
-                ...$this->recursiveExpandPath($pathElements),
-            ];
-        }
-
-        return $expandedPaths;
-    }
-
-    /**
-     * @param array<string, mixed> $config
-     *
-     * @return string[]
-     */
-    private function recursiveFlattenPaths(array $config, string $path = ''): array
-    {
-        $paths = [];
-
-        foreach ($config as $key => $value) {
-            if (is_array($value) && count($value) > 0) {
-                $paths = [
-                    ...$paths,
-                    ...$this->recursiveFlattenPaths($value, '/' . $key . $path),
-                ];
-
-                continue;
-            }
-
-            $paths[] = $key . $path;
-        }
-
-        return $paths;
-    }
-
-    /**
-     * @param array<string, mixed> $config
-     *
-     * @return array<string, mixed>
-     */
-    private function normalizeConfiguration(array $config): array
-    {
-        $normalizedConfig = [];
-
-        foreach ($config as $key => $value) {
-            $normalizedConfig[$key] = $this->recursiveNormalizeConfiguration($value);
-        }
-
-        return $normalizedConfig;
-    }
-
-    /**
-     * @param array<string, mixed> $config
-     *
-     * @return array<string, mixed>
-     */
-    private function recursiveNormalizeConfiguration(array $config): array
-    {
-        $normalizedConfig = [];
-
-        foreach ($config as $key => $value) {
-            if ($key === 'indexed') {
-                continue;
-            }
-
-            if ($key === 'children') {
-                return $this->recursiveNormalizeConfiguration($value);
-            }
-
-            $normalizedConfig[$key] = $this->recursiveNormalizeConfiguration($value);
-        }
-
-        return $normalizedConfig;
     }
 }
