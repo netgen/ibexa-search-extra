189ed5c47bb8c42ec3165b70944468a92a8125da
Petar Španja <petar@spanja.info>
2024-06-07 13:36:41 +0200

    NGSTACK-836: implement configurable Solr field handlers for descendant indexing

--- lib/Container/Compiler/ParentChildIndexingPass.php
+++ lib/Container/Compiler/ParentChildIndexingPass.php
@@ -6,41 +6,72 @@ namespace Netgen\IbexaSearchExtra\Container\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 
 class ParentChildIndexingPass implements CompilerPassInterface
 {
-    private const ParentChildIndexerTag = 'netgen.parent_child_indexer.message_handler';
+    private const ParentChildConfigurationParameter = 'netgen.ibexa_search_extra.parent_child_indexing.configuration';
+    private const ParentChildMessageHandlerTag = 'netgen.ibexa_search_extra.parent_child_indexing.message_handler';
     private const MessageHandlerTag = 'messenger.message_handler';
+    private const ParentChildSolrContentFieldMapperServiceId = 'netgen.ibexa_search_extra.solr.field_mapper.parent_child_indexing.content';
+    private const ParentChildSolrContentFieldMapperTag = 'netgen.ibexa_search_extra.solr.field_mapper.parent_child_indexing.content';
+    private const ParentChildSolrContentTranslationFieldMapperServiceId = 'netgen.ibexa_search_extra.solr.field_mapper.parent_child_indexing.content.translation';
+    private const ParentChildSolrContentTranslationFieldMapperTag = 'netgen.ibexa_search_extra.solr.field_mapper.parent_child_indexing.content.translation';
+    private const ParentChildSolrLocationFieldMapperServiceId = 'netgen.ibexa_search_extra.solr.field_mapper.parent_child_indexing.location';
+    private const ParentChildSolrLocationFieldMapperTag = 'netgen.ibexa_search_extra.solr.field_mapper.parent_child_indexing.location';
 
     public function process(ContainerBuilder $container): void
     {
-        $useParentChildIndexing = $container->getParameter(
-            'netgen_ibexa_search_extra.use_parent_child_indexing',
-        );
+        $configuration = $container->getParameter(self::ParentChildConfigurationParameter);
+        $isEnabled = $configuration['enabled'] ?? false;
 
-        if ($useParentChildIndexing !== true) {
+        if (!$isEnabled) {
             return;
         }
 
-        $serviceIds =$container->findTaggedServiceIds(self::ParentChildIndexerTag);
+        $this->registerHandlers($container);
+        $this->registerSolrContentFieldMappers($container);
+        $this->registerSolrContentTranslationFieldMappers($container);
+        $this->registerSolrLocationFieldMappers($container);
+    }
+
+    private function registerHandlers(ContainerBuilder $container): void
+    {
+        $serviceIds = $container->findTaggedServiceIds(self::ParentChildMessageHandlerTag);
 
         foreach ($serviceIds as $serviceId => $tag) {
             $definition = $container->getDefinition($serviceId);
             $definition->addTag(self::MessageHandlerTag);
         }
+    }
 
-        $parentChildIndexingUseDefaultSolrFullTextFieldMapper = $container->getParameter(
-            'netgen_ibexa_search_extra.parent_child_indexing_use_default_solr_fulltext_field_mapper',
-        );
+    private function registerSolrContentFieldMappers(ContainerBuilder $container): void
+    {
+        $definition = $container->getDefinition(self::ParentChildSolrContentFieldMapperServiceId);
+        $serviceIds = $container->findTaggedServiceIds(self::ParentChildSolrContentFieldMapperTag);
 
-        if ($parentChildIndexingUseDefaultSolrFullTextFieldMapper !== true) {
-            return;
+        foreach (array_keys($serviceIds) as $id) {
+            $definition->addMethodCall('addFieldMapper', [new Reference($id)]);
         }
+    }
 
-        $solrFullTextFieldMapper = $container->getDefinition(
-            'netgen_search_extra.parent_child_indexing.field_mapper.fulltext'
-        );
+    private function registerSolrContentTranslationFieldMappers(ContainerBuilder $container): void
+    {
+        $definition = $container->getDefinition(self::ParentChildSolrContentTranslationFieldMapperServiceId);
+        $serviceIds = $container->findTaggedServiceIds(self::ParentChildSolrContentTranslationFieldMapperTag);
 
-        $solrFullTextFieldMapper->addTag('ibexa.search.solr.field.mapper.content.translation');
+        foreach (array_keys($serviceIds) as $id) {
+            $definition->addMethodCall('addFieldMapper', [new Reference($id)]);
+        }
+    }
+
+    private function registerSolrLocationFieldMappers(ContainerBuilder $container): void
+    {
+        $definition = $container->getDefinition(self::ParentChildSolrLocationFieldMapperServiceId);
+        $serviceIds = $container->findTaggedServiceIds(self::ParentChildSolrLocationFieldMapperTag);
+
+        foreach (array_keys($serviceIds) as $id) {
+            $definition->addMethodCall('addFieldMapper', [new Reference($id)]);
+        }
     }
 }
