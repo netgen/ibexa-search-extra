b5d66b4a9bb41540af73a500ae93b24a46c14a33
Katarina Miočić <katarina.m@netgen.io>
2024-08-16 16:29:36 +0200

    NGSTACK-843 register elasticsearch services in compiler pass

--- lib/Container/Compiler/ElasticsearchExtensibleDocumentFactoryPass.php
+++ lib/Container/Compiler/ElasticsearchExtensibleDocumentFactoryPass.php
@@ -7,6 +7,8 @@ namespace Netgen\IbexaSearchExtra\Container\Compiler;
 use Ibexa\Contracts\Core\Persistence\Content\Handler;
 use Ibexa\Elasticsearch\DocumentMapper\DocumentFactoryInterface;
 use Netgen\IbexaSearchExtra\Core\Search\Elasticsearch\DocumentMapper\DocumentFactory;
+use Netgen\IbexaSearchExtra\Core\Search\Elasticsearch\Query\CriterionVisitor\Content\VisibilityVisitor as ContentVisibilityVisitor;
+use Netgen\IbexaSearchExtra\Core\Search\Elasticsearch\Query\CriterionVisitor\Location\VisibilityVisitor as LocationVisibilityVisitor;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
@@ -28,8 +30,34 @@ final class ElasticsearchExtensibleDocumentFactoryPass implements CompilerPassIn
         $this->processVisitors($container, 'content_translation');
         $this->processVisitors($container, 'location');
         $this->processVisitors($container, 'location_translation');
+
+        $this->processDocumentFactory($container);
+
+        $container
+            ->register(ContentVisibilityVisitor::class, ContentVisibilityVisitor::class)
+            ->addTag('ibexa.search.elasticsearch.query.content.criterion.visitor');
+
+        $container
+            ->register(LocationVisibilityVisitor::class, LocationVisibilityVisitor::class)
+            ->addTag('ibexa.search.elasticsearch.query.location.criterion.visitor');
     }
 
+    public function processDocumentFactory(ContainerBuilder $container): void
+    {
+        $container
+            ->register(DocumentFactory::class, DocumentFactory::class)
+            ->setDecoratedService(DocumentFactoryInterface::class)
+            ->setArguments([
+                new Reference('.inner'),
+                new Reference(Handler::class),
+                new Reference('netgen.ibexa_search_extra.elasticsearch.field_mapper.content.aggregate'),
+                new Reference('netgen.ibexa_search_extra.elasticsearch.field_mapper.location.aggregate'),
+                new Reference('netgen.ibexa_search_extra.elasticsearch.field_mapper.content_translation.aggregate'),
+                new Reference('netgen.ibexa_search_extra.elasticsearch.field_mapper.location_translation.aggregate'),
+                new Reference('netgen.ibexa_search_extra.elasticsearch.field_mapper.block.aggregate'),
+                new Reference('netgen.ibexa_search_extra.elasticsearch.field_mapper.block_translation.aggregate'),
+            ]);
+    }
 
     private function processVisitors(ContainerBuilder $container, string $name): void
     {
