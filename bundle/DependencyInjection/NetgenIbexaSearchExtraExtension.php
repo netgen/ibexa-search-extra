4302f4d4af0d3f7b1134c95a3ab0d175ff9caf48
Petar Španja <petar@spanja.info>
2025-05-29 06:39:06 +0200

    NGSTACK-843: fix CS

--- bundle/DependencyInjection/NetgenIbexaSearchExtraExtension.php
+++ bundle/DependencyInjection/NetgenIbexaSearchExtraExtension.php
@@ -10,8 +10,8 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-
 use Symfony\Component\Yaml\Yaml;
+
 use function array_key_exists;
 
 class NetgenIbexaSearchExtraExtension extends Extension implements PrependExtensionInterface
