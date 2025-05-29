9b69a78e06b52565a65b930b178f55cea33b6068
Petar Španja <petar@spanja.info>
2025-05-29 15:45:30 +0200

    NGSTACK-843: extract service refactoring

--- lib/Core/Search/Elasticsearch/DocumentMapper/BlockTranslationFieldMapper/BlockPageTextFieldMapper.php
+++ lib/Core/Search/Elasticsearch/DocumentMapper/BlockTranslationFieldMapper/BlockPageTextFieldMapper.php
@@ -5,12 +5,8 @@ declare(strict_types=1);
 namespace Netgen\IbexaSearchExtra\Core\Search\Elasticsearch\DocumentMapper\BlockTranslationFieldMapper;
 
 use Ibexa\Contracts\Core\Persistence\Content;
-use Ibexa\Contracts\Core\Persistence\Content\Type\Handler as ContentTypeHandler;
 use Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException;
-use Ibexa\Contracts\Core\Search\Field;
-use Ibexa\Contracts\Core\Search\FieldType\FullTextField;
-use Netgen\IbexaSearchExtra\Core\Search\Common\PageIndexing\ConfigResolver;
-use Netgen\IbexaSearchExtra\Core\Search\Common\PageIndexing\TextExtractor;
+use Netgen\IbexaSearchExtra\Core\Search\Common\PageIndexing\FieldMapper;
 use Netgen\IbexaSearchExtra\Core\Search\Elasticsearch\DocumentMapper\BlockTranslationFieldMapper;
 
 use function in_array;
@@ -18,9 +14,7 @@ use function in_array;
 class BlockPageTextFieldMapper extends BlockTranslationFieldMapper
 {
     public function __construct(
-        private readonly TextExtractor $pageTextExtractor,
-        private readonly ContentTypeHandler $contentTypeHandler,
-        private readonly ConfigResolver $configResolver,
+        private readonly FieldMapper $fieldMapper,
         private readonly bool $isEnabled,
     ) {}
 
@@ -34,27 +28,6 @@ class BlockPageTextFieldMapper extends BlockTranslationFieldMapper
      */
     public function mapFields(Content $content, string $languageCode): array
     {
-        $contentInfo = $content->versionInfo->contentInfo;
-        $contentType = $this->contentTypeHandler->load($content->versionInfo->contentInfo->contentTypeId);
-        $contentTypeIdentifier = $contentType->identifier;
-
-        $config = $this->configResolver->getSiteConfigForContent($contentInfo->id, $languageCode);
-
-        if (!in_array($contentTypeIdentifier, $config->getAllowedContentTypes(), true)) {
-            return [];
-        }
-
-        $text = $this->pageTextExtractor->extractPageText($contentInfo->id, $languageCode);
-        $fields = [];
-
-        foreach ($text as $level => $value) {
-            $fields[] = new Field(
-                'page_text_' . $level,
-                $value,
-                new FullTextField(),
-            );
-        }
-
-        return $fields;
+        return $this->fieldMapper->mapFields($content, $languageCode);
     }
 }
--- lib/Core/Search/Solr/FieldMapper/ContentTranslation/ContentPageTextFieldMapper.php
+++ lib/Core/Search/Solr/FieldMapper/ContentTranslation/ContentPageTextFieldMapper.php
@@ -4,22 +4,14 @@ declare(strict_types=1);
 
 namespace Netgen\IbexaSearchExtra\Core\Search\Solr\FieldMapper\ContentTranslation;
 
-use eZ\Publish\Core\Persistence\Legacy\Content\Type\Handler;
 use Ibexa\Contracts\Core\Persistence\Content;
-use Ibexa\Contracts\Core\Search\Field;
-use Ibexa\Contracts\Core\Search\FieldType\FullTextField;
 use Ibexa\Contracts\Solr\FieldMapper\ContentTranslationFieldMapper;
-use Netgen\IbexaSearchExtra\Core\Search\Common\PageIndexing\ConfigResolver;
-use Netgen\IbexaSearchExtra\Core\Search\Common\PageIndexing\TextExtractor;
-
-use function in_array;
+use Netgen\IbexaSearchExtra\Core\Search\Common\PageIndexing\FieldMapper;
 
 class ContentPageTextFieldMapper extends ContentTranslationFieldMapper
 {
     public function __construct(
-        private readonly TextExtractor $pageTextExtractor,
-        private readonly ConfigResolver $configResolver,
-        private readonly Handler $contentTypeHandler,
+        private readonly FieldMapper $fieldMapper,
         private readonly bool $isEnabled,
     ) {}
 
@@ -33,27 +25,6 @@ class ContentPageTextFieldMapper extends ContentTranslationFieldMapper
      */
     public function mapFields(Content $content, $languageCode): array
     {
-        $contentInfo = $content->versionInfo->contentInfo;
-        $contentType = $this->contentTypeHandler->loadByIdentifier($contentInfo->contentTypeId);
-        $contentTypeIdentifier = $contentType->identifier;
-
-        $config = $this->configResolver->getSiteConfigForContent($contentInfo->id, $languageCode);
-
-        if (!in_array($contentTypeIdentifier, $config->getAllowedContentTypes(), true)) {
-            return [];
-        }
-
-        $text = $this->pageTextExtractor->extractPageText($contentInfo->id, $languageCode);
-        $fields = [];
-
-        foreach ($text as $level => $value) {
-            $fields[] = new Field(
-                'page_text_' . $level,
-                $value,
-                new FullTextField(),
-            );
-        }
-
-        return $fields;
+        return $this->fieldMapper->mapFields($content, $languageCode);
     }
 }
