a86a9c9101ca354d642e1556a45d7d175beda9ac
Petar Španja <petar@spanja.info>
2025-05-31 17:09:24 +0200

    NGSTACK-843: use ContentInfo instead of Content ID

--- lib/Core/Search/Common/PageIndexing/TextExtractor.php
+++ lib/Core/Search/Common/PageIndexing/TextExtractor.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Netgen\IbexaSearchExtra\Core\Search\Common\PageIndexing;
 
+use Ibexa\Contracts\Core\Persistence\Content\ContentInfo;
+
 abstract class TextExtractor
 {
-    abstract public function extractPageText(int $contentId, string $languageCode);
+    abstract public function extractPageText(ContentInfo $contentInfo, string $languageCode);
 }
