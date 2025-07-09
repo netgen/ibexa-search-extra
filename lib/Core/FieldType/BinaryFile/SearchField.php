d1056d02843f1995159000e6faae993838215274
Ante Prkačin <ante.prkacin03@gmail.com>
2025-07-09 15:42:15 +0200

    NGSTACK-809 override Indexable BinaryFile SearchField by adding 'file_text' key

---
+++ lib/Core/FieldType/BinaryFile/SearchField.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\IbexaSearchExtra\Core\FieldType\BinaryFile;
+
+use Ibexa\Contracts\Core\FieldType\Indexable;
+use Ibexa\Contracts\Core\Persistence\Content\Field;
+use Ibexa\Contracts\Core\Persistence\Content\Type\FieldDefinition;
+use Ibexa\Contracts\Core\Search;
+use Netgen\IbexaSearchExtra\Core\Search\Common\PageIndexing\TextExtractor\FileTextExtractor;
+
+use function trim;
+
+/**
+ * Indexable definition for BinaryFile field type.
+ */
+final class SearchField implements Indexable
+{
+    /** @var array<int, array<int, string>> */
+    private array $cache = [];
+
+    public function __construct(
+        private readonly Indexable $innerField,
+        private readonly FileTextExtractor $fileTextExtractor,
+    ) {}
+
+    public function getIndexData(Field $field, FieldDefinition $fieldDefinition): array
+    {
+        $searchFields = $this->innerField->getIndexData($field, $fieldDefinition);
+
+        $text = $this->extractText($field);
+
+        if ($text !== '') {
+            $searchFields[] = new Search\Field(
+                'file_text',
+                $text,
+                new Search\FieldType\FullTextField(),
+            );
+        }
+
+        return $searchFields;
+    }
+
+    public function getIndexDefinition(): array
+    {
+        $innerDef = $this->innerField->getIndexDefinition();
+        $innerDef['file_text'] = new Search\FieldType\FullTextField();
+
+        return $innerDef;
+    }
+
+    public function getDefaultMatchField(): ?string
+    {
+        return $this->innerField->getDefaultMatchField();
+    }
+
+    public function getDefaultSortField(): ?string
+    {
+        return $this->innerField->getDefaultSortField();
+    }
+
+    private function extractText(Field $field): string
+    {
+        if (!isset($this->cache[$field->id][$field->versionNo])) {
+            $this->cache = [];
+            $this->cache[$field->id][$field->versionNo] = trim($this->fileTextExtractor->extractFromPersistenceField($field));
+        }
+
+        return $this->cache[$field->id][$field->versionNo];
+    }
+}
